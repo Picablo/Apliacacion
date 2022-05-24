76e80582e52abeba4b595c4ef2ffc89aca918471
Picablo <paulundernet@gmail.com>
2022-05-24 23:03:49 +0200

    Version 1.2.4

--- src/main/resources/static/js/registrar.js
+++ src/main/resources/static/js/registrar.js
@@ -33,5 +33,5 @@ async function registrarUsuario(){
         body: JSON.stringify(datos)
     });
     alert("La cuenta fue creada con exito");
-    window.location.href = 'usuarios.html';
+    window.location.href = 'principal.html';
 }
