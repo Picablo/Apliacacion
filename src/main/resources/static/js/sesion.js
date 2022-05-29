3039dfd676424f738968059f7d9a8cd796cc42e3
Picablo <paulundernet@gmail.com>
2022-05-29 15:14:04 +0200

    Version 1.2.7

--- src/main/resources/static/js/sesion.js
+++ src/main/resources/static/js/sesion.js
@@ -1,52 +1,33 @@
 $(document).ready(function() {
 });
 
-let imagenSesion
+let imagenSesion;
+let nombreSesion;
 
 if(localStorage.token == null){
     /*El token no existe*/
     let html = '<a class="dropdown-item" href="login.html"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>Iniciar Sesión</a>';
     document.getElementById("divUsuLogin").innerHTML = html;
     imagenSesion = "img/undraw_withoutlogin.svg";
-    document.getElementById("spanName").innerHTML= "";
+    nombreSesion = "";
     var URLactual = window.location;
     if(URLactual != "http://localhost:8080/login.html"){
         window.location.href = 'login.html';
     }
 }else{
 
-
+    imagenSesion = "img/undraw_profile_1.svg";
+    nombreSesion = "Usuario Logueado";
 
 
 }
 
 document.getElementById("imgSesion").src = imagenSesion;
+document.getElementById("spanName").innerHTML= nombreSesion;
 
-/*
-function crearSesion(){
-
-    usuarios = await fetch('api/usuarios/usuid', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    });
-
-    userID = await usuarios.json();
-
-    for (let usuario of userID) {
-        $('#txtUserId').append("<option value="+ usuario.id+">"+usuario.nombre+" "+usuario.apellido+"</option>");
-    }
-
-
-
-        let request = await fetch('api/jwt/'+ localStorage.getItem("token"), {
-           method: 'GET',
-           headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-                },
-       });
-       const id = await request.json();
-}*/
+function cerrarSesion(){
+    sessionStorage.clear();
+    localStorage.clear();
+    window.location.href = 'login.html';
+    alert("Cerrar Sesion");
+}
